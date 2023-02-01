adf4e1e5f69656e6db405f43c117b808f0708324
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2023-02-01 17:00:01 +0600

    Create product card.

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,129 @@
-<script setup></script>
+<script setup>
+import AppCardBg from "@/components/AppCardBg.vue";
+</script>
 
 <template>
-  <div>Card</div>
+  <div :class="$style.card">
+    <div :class="$style.cardMain">
+      <AppCardBg />
+
+      <div :class="$style.cardLabelBubble">
+        <p :class="$style.cardLabelBubbleText">
+          0,5
+          <br />
+          <span>кг</span>
+        </p>
+      </div>
+
+      <div :class="$style.cardContent">
+        <p :class="$style.cardSupTitle">Сказочное заморское яство</p>
+        <h3 :class="$style.cardMainTitle">Нямушка</h3>
+        <h4 :class="$style.cardMainSubTitle">с фуа-гра</h4>
+        <p :class="$style.cardInfoText">
+          10 порций <br />
+          мышь в подарок
+        </p>
+      </div>
+    </div>
+    <p :class="$style.cardDesc">
+      Чего сидишь? Порадуй котэ,
+      <button type="button" class="link">купи.</button>
+    </p>
+  </div>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.card {
+  width: 320px;
+  position: relative;
+  font-family: $app-secondary-font-family;
+}
+
+.cardMain {
+  position: relative;
+  color: $card-text-color;
+}
+
+.cardLabelBubble {
+  position: absolute;
+  height: 80px;
+  width: 80px;
+  right: 16px;
+  bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: $card-bubble-label-bg-color;
+  border-radius: 50%;
+}
+
+.cardLabelBubbleText {
+  position: relative;
+  top: 4px;
+  margin: 0;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 42px;
+  line-height: 0.5;
+  color: $card-bubble-label-color;
+
+  span {
+    font-size: 21px;
+  }
+}
+
+.cardContent {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  padding: 21px 50px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: $card-secondary-text-color;
+}
+
+.cardSupTitle {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  color: $card-secondary-text-color;
+  margin: 0 0 5px 0;
+}
+
+.cardMainTitle {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 56px;
+  color: #000000;
+  margin: 0;
+}
+
+.cardMainSubTitle {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+  margin: 0 0 15px 0;
+}
+
+.cardInfoText {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.14;
+  color: $card-secondary-text-color;
+  margin: 0 0 15px 0;
+}
+
+.cardDesc {
+  text-align: center;
+  margin: 14px 0 0 0;
+}
+</style>
---
+++ src/components/AppCardBg.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg viewBox="0 0 320 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <!-- Card path area -->
+      <path
+        :id="cardPathId"
+        d="M0 42.6762V468C0 474.627 5.37259 480 12 480H308C314.627 480 320 474.627 320 468V12C320 5.37258 314.627 0 308 0H42.6762L0 42.6762Z"
+      />
+
+      <clipPath :id="cardClipAreaId">
+        <use :href="`#${cardPathId}`" />
+      </clipPath>
+    </defs>
+
+    <g :clip-path="`url(#${cardClipAreaId})`">
+      <!-- Card bg -->
+      <use :href="`#${cardPathId}`" fill="#F2F2F2" />
+
+      <image
+        href="@/assets/card-item-img.png"
+        height="100%"
+        width="100%"
+        preserveAspectRatio="xMinYMax meet"
+      />
+
+      <!-- Card border -->
+      <use
+        :href="`#${cardPathId}`"
+        fill="none"
+        stroke="#1698D9"
+        stroke-width="8px"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cardPathId: null,
+      cardClipAreaId: null,
+    };
+  },
+  mounted() {
+    let uid = this.$.uid;
+
+    this.cardPathId = `card_area-${uid}`;
+    this.cardClipAreaId = `card_clip_area-${uid}`;
+  },
+};
+</script>
+
+<style lang="scss" module></style>
