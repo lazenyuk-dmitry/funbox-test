8606d6dca05348956a47ed67b40b388793807680
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2023-02-01 17:49:22 +0600

    Setup mobile media

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,54 @@ import AppCard from "@/components/AppCard.vue";
 </script>
 
 <template>
-  <main>
+  <main class="container" :class="$style.main">
     <h1>Ты сегодня покормил кота?</h1>
 
-    <AppCard />
+    <div :class="$style.productsSection">
+      <div :class="$style.cardWrap">
+        <AppCard :class="$style.productCard" />
+      </div>
+      <div :class="$style.cardWrap">
+        <AppCard :class="$style.productCard" />
+      </div>
+      <div :class="$style.cardWrap">
+        <AppCard :class="$style.productCard" />
+      </div>
+    </div>
   </main>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.productsSection {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 0 60px;
+
+  @media (max-width: $media-smartphone-xl) {
+    padding-left: 0;
+    padding-right: 0;
+  }
+}
+
+.cardWrap {
+  flex-basis: 33%;
+  padding: 0 20px;
+
+  @media (max-width: $media-hi-res) {
+    flex-basis: 50%;
+
+    &:first-child {
+      flex-basis: 100%;
+    }
+  }
+
+  @media (max-width: $media-smartphone-xl) {
+    flex-basis: 100%;
+  }
+}
+
+.productCard {
+  margin: 0 auto 40px;
+}
+</style>
